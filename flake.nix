0eb3dea503548c455f58779f26a4639068adfca4
Cole Mickens <cole.mickens@gmail.com>
2020-11-05 15:19:29 -0800

    update.sh: try to fix nix-build-uncached usage

--- flake.nix
+++ flake.nix
@@ -3,11 +3,10 @@
 
   # TODO: should warn whenever flakes are resolved to different versions (names of flakes should match repo names?)
   inputs = {
-    master = { url = "github:nixos/nixpkgs/master"; };
     nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
-    cachixpkgs = { url = "github:nixos/nixpkgs/nixos-20.03"; };
+    cmpkgs = { url = "github:colemickens/nixpkgs/cmpkgs"; };# TODO: remove
+    cachixpkgs = { url = "github:nixos/nixpkgs/nixos-20.09"; };
     mozilla = { url = "github:colemickens/nixpkgs-mozilla"; flake = false; };
-    flake-utils = { url = "github:numtide/flake-utils"; }; # TODO: adopt this
   };
 
   outputs = inputs:
@@ -16,7 +15,8 @@
 
       nameValuePair = name: value: { inherit name value; };
       genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
-      forAllSystems = genAttrs [ "x86_64-linux" "i686-linux" "aarch64-linux" ];
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = genAttrs supportedSystems;
 
       pkgsFor = pkgs: system:
         import pkgs {
@@ -24,6 +24,7 @@
           config.allowUnfree = true;
           overlays = [ (import "${inputs.mozilla}/firefox-overlay.nix") ];
         };
+      pkgs_ = genAttrs (builtins.attrNames inputs) (inp: genAttrs supportedSystems (sys: pkgsFor inputs."${inp}" sys));
 
       # impure, but that's by design
       sysPkgs = (pkgsFor inputs.nixpkgs builtins.currentSystem);
@@ -41,28 +42,17 @@
       };
     in
     rec {
-      devShell =
-       forAllSystems (system:
-        let 
-          master_ = pkgsFor inputs.master system;
-          nixpkgs_ = pkgsFor inputs.nixpkgs system;
-          cachixpkgs_ = pkgsFor inputs.cachixpkgs system;
-        in nixpkgs_.mkShell {
-          nativeBuildInputs = with nixpkgs_; [
-            bash cacert curl git jq openssh ripgrep
-            cachixpkgs_.cachix
-            master_.nixFlakes
-            master_.nix-build-uncached
-          ];
+      devShell = forAllSystems (system:
+        pkgs_.nixpkgs.${system}.mkShell {
+          nativeBuildInputs = []
+            ++ (with pkgs_.cachixpkgs.${system}; [ cachix ])
+            ++ (with pkgs_.nixpkgs.${system}; [ bash cacert curl git jq mercurial openssh ripgrep ])
+            ++ (with pkgs_.cmpkgs.${system}; [ nixUnstable nix-prefetch nix-build-uncached ])
+          ;
         }
       );
 
-      packages = forAllSystems (system:
-        let
-          nixpkgs_ = (pkgsFor inputs.nixpkgs system);
-          attrValues = inputs.nixpkgs.lib.attrValues;
-        in (variants system)
-      );
+      packages = forAllSystems (system: variants system);
 
       latest =
         let
--- update.sh
+++ update.sh
@@ -23,17 +23,27 @@ nix --experimental-features 'nix-command flakes' \
 newversion="$(cat latest.json | jq -r '.cachedInfo.chksum' |  grep -Eo '[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}')"
 
 out="$(mktemp -d)"
-nix-build-uncached \
-  --option "extra-binary-caches" "https://cache.nixos.org https://nixpkgs-wayland.cachix.org" \
-  --option "trusted-public-keys" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA=" \
-  --option "build-cores" "0" \
-  --option "narinfo-cache-negative-ttl" "0" \
-  --out-link "${out}/result" packages.nix
-results=(); shopt -s nullglob
-for f in ${out}/result*; do
-  results=("${results[@]}" "${f}")
-done
-echo "${results[@]}" | cachix push "${cache}"
+nix-build-uncached -build-flags "\
+  --experimental-features 'nix-command flakes ca-references' \
+  --option 'extra-binary-caches' 'https://cache.nixos.org https://nixpkgs-wayland.cachix.org' \
+  --option 'trusted-public-keys' 'cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA=' \
+  --option 'build-cores' '0' \
+  --option 'narinfo-cache-negative-ttl' '0' \
+  --out-link '${out}/result'" \
+    packages.nix
+
+# results=(); shopt -s nullglob
+# for f in ${out}/result*; do
+#   results=("${results[@]}" "${f}")
+# done
+# echo "${results[@]}" | cachix push "${cache}"
+
+if find ${out} | grep result; then
+  nix --experimental-features 'nix-command flakes' \
+    path-info --json -r ${out}/result* > ${out}/path-info.json
+  jq -r 'map(select(.ca == null and .signatures == null)) | map(.path) | .[]' < "${out}/path-info.json" > "${out}/paths"
+  cachix push "${cache}" < "${out}/paths"
+fi
 
 if [[ "${newversion}" != "${oldversion}" ]]; then
   commitmsg="firefox-nightly-bin: ${oldversion} -> ${newversion}"
