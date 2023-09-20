c3ece348c377679dd419778c8bf9031881527196
Artturin <Artturin@artturin.com>
2023-09-20 23:33:41 +0300

    move functions in to eachSystem let

--- flake.nix
+++ flake.nix
@@ -20,78 +20,78 @@
         "aarch64-linux" = "linux-aarch64"; # TODO: doesn't work since Moz doesn't publish 'em
       };
 
-      nameValuePair = name: value: { inherit name value; };
-      genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
       # supportedSystems = [ "x86_64-linux" "aarch64-linux" ]; # TODO: still not there
       supportedSystems = [ "x86_64-linux" ];
 
-      pkgsFor = pkgs: system:
+    in
+    lib.flake-utils.eachSystem supportedSystems (system: let
+      pkgsFor = pkgs: overlays:
         import pkgs {
           inherit system;
           config.allowUnfree = true;
           overlays = [ (import "${inputs.mozilla}/firefox-overlay.nix") ];
         };
-      pkgs_ = genAttrs (builtins.attrNames inputs) (inp: genAttrs supportedSystems (sys: pkgsFor inputs."${inp}" sys));
 
-      fv = system: pkgs_.nixpkgs.${system}.lib.firefoxOverlay.firefoxVariants;
-      variants = system: (builtins.mapAttrs
+      pkgs_ = lib.genAttrs (builtins.attrNames inputs) (inp: pkgsFor inputs."${inp}" [ ]);
+
+      fv = pkgs_.nixpkgs.lib.firefoxOverlay.firefoxVariants;
+      variants = (builtins.mapAttrs
         (n: v:
           let
             cv = metadata.${system}."variants".${n};
             cvi = metadata.${system}."versionInfo".${n};
           in
-            pkgs_.nixpkgs."${system}".lib.firefoxOverlay.firefoxVersion (cv // { info = cvi; })
+            pkgs_.nixpkgs.lib.firefoxOverlay.firefoxVersion (cv // { info = cvi; })
         )
-        (fv system)
+        (fv)
       );
 
       # latest versionInfo outputs for each variant
       # impure, but by design. this is stored/recorded and then used purely
-      impureVariants = system: (
-        (pkgs_.nixpkgs."${system}".lib.firefoxOverlay.firefoxVariants)
-      );
-      impureVersionInfos = system: (builtins.mapAttrs
-        (n: v: pkgs_.nixpkgs."${system}".lib.firefoxOverlay.versionInfo (v // { system = mozillaSystemDict.${system};}))
-        (fv system)
+      impureVariants =
+        (pkgs_.nixpkgs.lib.firefoxOverlay.firefoxVariants);
+
+      impureVersionInfos = (builtins.mapAttrs
+        (n: v: pkgs_.nixpkgs.lib.firefoxOverlay.versionInfo (v // { system = mozillaSystemDict.${system};}))
+        (fv)
       );
 
       # https://nixos.org/manual/nixos/unstable/index.html#sec-calling-nixos-tests
       nixos-lib = import (inputs.nixpkgs + "/nixos/lib") { };
-      runNixOSTestFor = system: pkg: nixos-lib.runTest {
+      runNixOSTestFor = pkg: nixos-lib.runTest {
         imports = [ ./tests/firefox.nix ];
-        hostPkgs = pkgs_.nixpkgs."${system}";
+        hostPkgs = pkgs_.nixpkgs;
         defaults = {
-          nixpkgs.pkgs = pkgs_.nixpkgs."${system}";
+          nixpkgs.pkgs = pkgs_.nixpkgs;
           # Less dependencies
           documentation.enable = false;
         };
         _module.args.firefoxPackage = pkg;
       };
 
-    in
-    lib.flake-utils.eachSystem supportedSystems (system: {
-      devShell = pkgs_.nixpkgs.${system}.mkShell {
+    in {
+      devShell = pkgs_.nixpkgs.mkShell {
         nativeBuildInputs = []
-          ++ (with pkgs_.cachix.${system}; [ cachix ])
-          ++ (with pkgs_.nixpkgs.${system}; [
+          ++ (with pkgs_.cachix; [ cachix ])
+          ++ (with pkgs_.nixpkgs; [
               nixUnstable nix-prefetch nix-build-uncached
               bash cacert curl git jq mercurial openssh ripgrep
           ])
         ;
       };
 
-      packages = variants system;
+      packages = variants;
 
       latest = {
-        variants = impureVariants system;
-        versionInfo = impureVersionInfos system;
+        variants = impureVariants;
+        versionInfo = impureVersionInfos;
       };
 
-      defaultPackage = pkgs_.nixpkgs."${system}".symlinkJoin {
+      defaultPackage = pkgs_.nixpkgs.symlinkJoin {
           name = "flake-firefox-nightly";
-          paths = builtins.attrValues (variants system);
+          paths = builtins.attrValues (variants);
       };
 
-      checks = builtins.mapAttrs (_: value: runNixOSTestFor system value ) self.packages.${system};
+      checks = builtins.mapAttrs (_: value: runNixOSTestFor value ) self.packages.${system};
     });
 }
