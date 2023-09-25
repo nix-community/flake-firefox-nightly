ae5b5bf1a2f45f908a143100e728bddf0c0c7f2a
Artturin <Artturin@artturin.com>
2023-09-25 22:16:15 +0300

    add timeout to the test itself
    and add 5 minutes to the workflow timeout just in case of a slow machine

--- tests/firefox.nix
+++ tests/firefox.nix
@@ -17,6 +17,7 @@
       with subtest("Wait until Firefox has finished loading the Valgrind docs page"):
           machine.succeed(
               "systemd-run -E DISPLAY=:0 ${firefoxPackage.unwrapped.binaryName} file://${pkgs.valgrind.doc}/share/doc/valgrind/html/index.html"
+              , timeout=60
           )
           machine.wait_for_window("Valgrind")
           machine.sleep(20)
